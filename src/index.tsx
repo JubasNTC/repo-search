3b71fc726ca6048c1a8b4c1eb067f77559fcdceb
JubasNTC <jubas1337@mail.ru>
2020-11-03 15:46:57 +0300

    carry api data to env

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ import { App } from "./app/App";
 import { history } from "./app/history";
 
 const restLink = new RestLink({
-  uri: "https://api.github.com/search/repositories",
+  uri: process.env.REACT_APP_GITHUB_API_URL,
   headers: {
-    Authorization: `token 3d9ca2bd44d45d43323e47837638cbf5a3c484c9`,
+    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
 });
 
